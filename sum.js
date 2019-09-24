1cb17e4f25346b1b69d9d7f66f83f3d2d31f26b9
Mark Packham <markpackham1@gmail.com>
2019-09-24 18:08:06 +0100

    add unused function to trigger Jest into showing not every function in sum.js is being tested

--- sum.js
+++ sum.js
@@ -2,4 +2,9 @@ function sum(a, b) {
   return a + b;
 }
 
+// a function that doesn't get tested so Jest makes you aware that not every function is tested
+function helper() {
+  console.log("Helper");
+}
+
 module.exports = sum;
